884ca7a83686eed1cdca46cc2f944a1356fb4ed7
barbaroshayrettinekici <117522497+barbaroshayrettinekici@users.noreply.github.com>
2022-11-23 23:00:21 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
     //github 3. gün
-        //github ilk deneme
+    //github ilk deneme
     }
 
 }
